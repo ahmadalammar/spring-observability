4e315b38557ef3de1e7d361e554c3d268dcd2219
Alammarcr7 <ahmadalammar@airasia.com>
2023-10-30 22:33:27 +0800

    first commit

---
+++ src/main/java/com/alammar/observability/ObservabilityApplication.java
@@ -0,0 +1,11 @@
+package com.alammar.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alammar/observability/ObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alammar.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
